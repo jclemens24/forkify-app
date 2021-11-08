03a763a01d6e03e40f19f353c56310ef3f3e0180
jclemens24 <jclemens24@hotmail.com>
2021-11-08 17:35:19 -0500

    delete import

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,5 @@
 import View from './View';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
